04536c66be112e6e3c2000e02e89eadb0538e178
DraygoKorvan <draygokorvan@ps-roughriders.com>
2016-09-18 19:27:39 -0500

    Add files via upload

--- HUDApi.cs
+++ HUDApi.cs
@@ -3,36 +3,176 @@ using System;
 using VRageMath;
 using System.Text;
 using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
 namespace Draygo.API
 {
 
 	public class HUDTextNI
 	{
+		public Dictionary<char, int> FontDict = new Dictionary<char, int>()
+		{
+			{' ',15},
+			{'!',24},
+			{'\"',25},
+			{'#',35},
+			{'$',36},
+			{'%',39},
+			{'&',35},
+			{'\'',22},
+			{'(',24},
+			{')',24},
+			{'*',26},
+			{'+',34},
+			{',',25},
+			{'-',25},
+			{'.',25 },
+			{'/',30 },
+			{'0',35},
+			{'1',24},
+			{'2',34},
+			{'3',33},
+			{'4',34},
+			{'5',35},
+			{'6',35},
+			{'7',31},
+			{'8',35},
+			{'9',35},
+			{':',25},
+			{';',25},
+			{'<',34},
+			{'=',34},
+			{'>',34},
+			{'?',31},
+			{'@',40},
+			{'A',37},
+			{'B',37},
+			{'C',35},
+			{'D',37},
+			{'E',34},
+			{'F',32},
+			{'G',36},
+			{'H',35},
+			{'I',24},
+			{'J',31},
+			{'K',33},
+			{'L',30},
+			{'M',42},
+			{'N',37},
+			{'O',37},
+			{'P',35},
+			{'Q',37},
+			{'R',37},
+			{'S',37},
+			{'T',32},
+			{'U',36},
+			{'V',35},
+			{'W',47},
+			{'X',35},
+			{'Y',36},
+			{'Z',35},
+			{'[',25},
+			{'\\',28},
+			{']',25},
+			{'^',34},
+			{'_',31},
+			{'`',23},
+			{'a',33},
+			{'b',33},
+			{'c',32},
+			{'d',33},
+			{'e',33},
+			{'f',24},
+			{'g',33},
+			{'h',33},
+			{'i',23},
+			{'j',23},
+			{'k',32},
+			{'l',23},
+			{'m',42},
+			{'n',33},
+			{'o',33},
+			{'p',33},
+			{'q',33},
+			{'r',25},
+			{'s',33},
+			{'t',25},
+			{'u',33},
+			{'v',30},
+			{'w',42},
+			{'x',31},
+			{'y',33},
+			{'z',31},
+			{'{',25},
+			{'|',22},
+			{'}',25},
+			{'~',34}
+		};
 		public enum TextOrientation : byte
 		{
 			ltr = 1,
 			center = 2,
 			rtl = 3
 		}
+		[Flags]
+		public enum Options : byte
+		{
+			None = 0x0,
+			HideHud = 0x1,
+			Shadowing = 0x2,
+		}
 		public struct HUDMessage
 		{
 			public long id;
 			public int ttl;
-			public Vector2D Origin;
+			public Vector2D origin;
 			public string message;
+			public Options options;
+			public Vector4 shadowcolor;
+			public double scale;
 
 			/// <summary>
 			/// Data to transport for HUD Text Display
 			/// </summary>
 			/// <param name="messageid">Message id is automatically generated if set to 0. Resend a message with the same ID to overwrite previously sent messages</param>
 			/// <param name="timetolive">How many frames a message will live</param>
-			/// <param name="origin">Vector 2D, middle of screen is (0,0) up is positive, down is negative, right is positive, left is negative.</param>
-			/// <param name="message">Actual message you want to send.&lt;color=colorname&gt; to change the color of the text.</param>
-			public HUDMessage(long messageid, int timetolive, Vector2D origin, string message)
+			/// <param name="Origin">Vector 2D, middle of screen is (0,0) up is positive, down is negative, right is positive, left is negative.</param>
+			/// <param name="message">Actual message you want to send. &lt;color=colorname&gt; to change the color of the text.</param>
+			public HUDMessage(long messageid, int timetolive, Vector2D Origin, string message)
 			{
 				id = messageid;
 				ttl = timetolive;
-				Origin = origin;
+				origin = Origin;
+				options = Options.None;
+				var blackshadow = Color.Black;
+				shadowcolor = blackshadow.ToVector4();
+				scale = 1;
+				this.message = message;
+			}
+			/// <summary>
+			/// Data to transport for HUD Text Display
+			/// </summary>
+			/// <param name="messageid">Message id is automatically generated if set to 0. Resend a message with the same ID to overwrite previously sent messages</param>
+			/// <param name="timetolive">How many frames a message will live</param>
+			/// <param name="Origin">Vector 2D, middle of screen is (0,0) up is positive, down is negative, right is positive, left is negative. up to +1 and -1</param>
+			/// <param name="Scale">Scale multiplier for text, 0.5 is half as big. 2.0 twice as big.</param>
+			/// <param name="HideHud">Automatically hide this HUD element when player hides their HUD</param>
+			/// <param name="Shadowing">Enables text shadowing</param>
+			/// <param name="Shadowcolor">Specifies Text Shadow Color</param>
+			/// <param name="message">Message string that you want to send. &lt;color=colorname&gt; to change the color of the text. </param>
+			public HUDMessage(long messageid, int timetolive, Vector2D Origin, double Scale, bool HideHud, bool Shadowing, Color Shadowcolor, string message)
+			{
+				options = Options.None;
+				if (HideHud)
+					options |= Options.HideHud;
+				if (Shadowing)
+					options |= Options.Shadowing;
+				scale = Scale;
+				id = messageid;
+				ttl = timetolive;
+				shadowcolor = Shadowcolor.ToVector4();
+				origin = Origin;
 				this.message = message;
 			}
 
@@ -46,7 +186,7 @@ namespace Draygo.API
 			public Vector3D left;
 			public double scale;
 			public string message;
-			public TextOrientation TOrientation;
+			public TextOrientation t_orientation;
 			/// <summary>
 			/// Data to transport for Message in 3D space
 			/// </summary>
@@ -66,7 +206,7 @@ namespace Draygo.API
 				pos = position;
 				up = Up;
 				left = Left;
-				TOrientation = orientation;
+				t_orientation = orientation;
 				this.message = message;
 			}
 		}
@@ -74,14 +214,14 @@ namespace Draygo.API
 		{
 			public long id;
 			public int ttl;
-			public long EntityId;
+			public long entityid;
 			public Vector3D rel;
 			public Vector3D up;
 			public Vector3D forward;
 			public double scale;
 			public string message;
-			public TextOrientation TOrientation;
-			public Vector2D Max;
+			public TextOrientation t_orientation;
+			public Vector2D max;
 			/// <summary>
 			/// Data to transport Entity attached message
 			/// </summary>
@@ -101,12 +241,12 @@ namespace Draygo.API
 				id = messageid;
 				ttl = timetolive;
 				this.scale = scale;
-				this.EntityId = EntityId;
+				this.entityid = EntityId;
 				rel = localposition;
 				up = Up;
 				forward = Forward;
-				Max = new Vector2D(max_x, max_y);
-				TOrientation = orientation;
+				max = new Vector2D(max_x, max_y);
+				t_orientation = orientation;
 				this.message = message;
 			}
 		}
@@ -133,6 +273,18 @@ namespace Draygo.API
 				m_heartbeat = value;
 			}
 		}
+		/// <summary>
+		/// True if HUD is visible, will also return true if Session is null
+		/// </summary>
+		public bool IsHudVisible
+		{
+			get
+			{
+
+				return (MyAPIGateway.Session?.Config?.MinimalHud == null || !MyAPIGateway.Session.Config.MinimalHud);
+			}
+			
+		}
 
 		/// <summary>
 		/// You must specify a modId to avoid conflicts with other mods. Just pick a random number, it probably will be fine ;) Please call .Close() during the cleanup of your mod.
@@ -156,6 +308,43 @@ namespace Draygo.API
 		public long GetNextID()
 		{
 			return currentId++;
+		}
+
+		/// <summary>
+		/// Returns the text length of text in meters. This method is not for HUDMessage. Returns a value at end of string or end of line ('\n')
+		/// </summary>
+		/// <param name="text">Text to measure</param>
+		/// <param name="scale">Scale of text</param>
+		/// <returns></returns>
+		public double GetLineLength(string text, double scale = 1.0d)
+		{
+			double retval = 0;
+			int value = 0;
+			Regex ColorEncoder = new Regex("<color=(\\w*?|\\d*?\\,\\d*?\\,\\d*?|\\d*?,\\d*?,\\d*?,\\d*?)>", RegexOptions.IgnoreCase);
+			var matches = ColorEncoder.Split(text);
+			int i = 0;
+			foreach (var match in matches)
+			{
+				i++;
+				if (i % 2 == 0)
+				{
+					continue;
+				}
+				foreach (var letter in match)
+				{
+					if (letter == '\n')
+					{
+						return retval;
+					}
+					if (FontDict.TryGetValue(letter, out value))
+					{
+						retval += ((double)value / 45d) * scale * 1.2d;
+					}
+					else
+						retval += (15d / 45d) * scale * 1.2d;
+				}
+			}
+			return retval;
 		}
 		/// <summary>
 		/// Creates and Sends a HUDMessage
@@ -190,7 +379,7 @@ namespace Draygo.API
 		public HUDMessage Send(HUDMessage message)
 		{
 			var msg = Encode(ref message);
-			MyAPIGateway.Multiplayer.SendMessageTo(HUDAPI_MESSAGE, msg, MyAPIGateway.Multiplayer.MyId, true);
+			MyAPIGateway.Multiplayer.SendMessageTo(HUDAPI_ADVMSG, msg, MyAPIGateway.Multiplayer.MyId, true);
 			return message;
 		}
 		/// <summary>
@@ -223,7 +412,7 @@ namespace Draygo.API
 		public HUDMessage SendToOthers(HUDMessage message)
 		{
 			var msg = Encode(ref message);
-			MyAPIGateway.Multiplayer.SendMessageToOthers(HUDAPI_MESSAGE, msg, true);
+			MyAPIGateway.Multiplayer.SendMessageToOthers(HUDAPI_ADVMSG, msg, true);
 			return message;
 		}
 		/// <summary>
@@ -250,26 +439,44 @@ namespace Draygo.API
 		}
 		private byte[] Encode(ref HUDMessage message)
 		{
+
+			ushort msgtype = 3;
 			if (message.id == 0)
 			{
 
 				message.id = GetNextID();
 			}
 
-
+			byte[] ver = BitConverter.GetBytes(MOD_VER);
+			byte[] type = BitConverter.GetBytes(msgtype);
 			byte[] modid = BitConverter.GetBytes(m_modId);
 			byte[] mid = BitConverter.GetBytes(message.id);
 			byte[] ttl = BitConverter.GetBytes(message.ttl);
-			byte[] vx = BitConverter.GetBytes(message.Origin.X);
-			byte[] vy = BitConverter.GetBytes(message.Origin.Y);
+			byte[] vx = BitConverter.GetBytes(message.origin.X);
+			byte[] vy = BitConverter.GetBytes(message.origin.Y);
+			byte[] options = new byte[1] { (byte)message.options };
+			byte[] cx = BitConverter.GetBytes(message.shadowcolor.X);
+			byte[] cy = BitConverter.GetBytes(message.shadowcolor.Y);
+			byte[] cz = BitConverter.GetBytes(message.shadowcolor.Z);
+			byte[] cw = BitConverter.GetBytes(message.shadowcolor.W);
+			byte[] scale = BitConverter.GetBytes(message.scale);
 			byte[] encode = Encoding.UTF8.GetBytes(message.message);
-			byte[] msg = new byte[modid.Length + mid.Length + ttl.Length + vx.Length + vy.Length + encode.Length];
-			modid.CopyTo(msg, 0);
-			mid.CopyTo(msg, modid.Length);
-			ttl.CopyTo(msg, modid.Length + mid.Length);
-			vx.CopyTo(msg, modid.Length + mid.Length + ttl.Length);
-			vy.CopyTo(msg, modid.Length + mid.Length + ttl.Length + vx.Length);
-			encode.CopyTo(msg, modid.Length + mid.Length + ttl.Length + vx.Length + vy.Length);
+			byte[] msg = new byte[ver.Length + type.Length + cx.Length * 4 + scale.Length + modid.Length + mid.Length + ttl.Length + vx.Length + vy.Length + encode.Length + options.Length];
+			int lth = 0;
+			Copy(ref msg, ref ver, ref lth);
+			Copy(ref msg, ref type, ref lth);
+			Copy(ref msg, ref modid, ref lth);
+			Copy(ref msg, ref mid, ref lth);
+			Copy(ref msg, ref ttl, ref lth);
+			Copy(ref msg, ref vx, ref lth);
+			Copy(ref msg, ref vy, ref lth);
+			Copy(ref msg, ref options, ref lth);
+			Copy(ref msg, ref cx, ref lth);
+			Copy(ref msg, ref cy, ref lth);
+			Copy(ref msg, ref cz, ref lth);
+			Copy(ref msg, ref cw, ref lth);
+			Copy(ref msg, ref scale, ref lth);
+			Copy(ref msg, ref encode, ref lth);
 			return msg;
 		}
 		private byte[] Encode(ref SpaceMessage message)
@@ -286,7 +493,7 @@ namespace Draygo.API
 			byte[] modid = BitConverter.GetBytes(m_modId);
 			byte[] mid = BitConverter.GetBytes(message.id);
 			byte[] ttl = BitConverter.GetBytes(message.ttl);
-			byte[] orient = new byte[1] { (byte)message.TOrientation };
+			byte[] orient = new byte[1] { (byte)message.t_orientation };
 			byte[] pos = Encode(message.pos);
 			byte[] up = Encode(message.up);
 			byte[] left = Encode(message.left);
@@ -307,9 +514,6 @@ namespace Draygo.API
 			Copy(ref msg, ref encode, ref lth);
 			return msg;
 		}
-
-
-
 		private byte[] Encode(ref EntityMessage message)
 		{
 			ushort msgtype = 2;
@@ -324,10 +528,10 @@ namespace Draygo.API
 			byte[] modid = BitConverter.GetBytes(m_modId);
 			byte[] mid = BitConverter.GetBytes(message.id);
 			byte[] ttl = BitConverter.GetBytes(message.ttl);
-			byte[] orient = new byte[1] { (byte)message.TOrientation };
-			byte[] entity = BitConverter.GetBytes(message.EntityId);
-			byte[] mx = BitConverter.GetBytes(message.Max.X);
-			byte[] my = BitConverter.GetBytes(message.Max.Y);
+			byte[] orient = new byte[1] { (byte)message.t_orientation };
+			byte[] entity = BitConverter.GetBytes(message.entityid);
+			byte[] mx = BitConverter.GetBytes(message.max.X);
+			byte[] my = BitConverter.GetBytes(message.max.Y);
 			byte[] rel = Encode(message.rel);
 			byte[] up = Encode(message.up);
 			byte[] forward = Encode(message.forward);
@@ -352,8 +556,6 @@ namespace Draygo.API
 			Copy(ref msg, ref encode, ref lth);
 			return msg;
 		}
-
-		
 		private byte[] Encode(Vector3D vec)
 		{
 			byte[] x = BitConverter.GetBytes(vec.X);
